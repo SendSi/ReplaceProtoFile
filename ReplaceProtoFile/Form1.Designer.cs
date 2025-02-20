58da1bb165c158ec0e578b5852c19fd40488ddb5
曾思信 <389585111@qq.com>
2025-02-20 18:03:14 +0800

    一版 可用

---
+++ ReplaceProtoFile/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ReplaceProtoFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.txtProtoPath = new System.Windows.Forms.TextBox();
+            this.lbl = new System.Windows.Forms.Label();
+            this.btnUnityReplaceFile = new System.Windows.Forms.Button();
+            this.btnReplaceTxt = new System.Windows.Forms.Button();
+            this.lblExcInfo = new System.Windows.Forms.Label();
+            this.txtUnityProtoPath = new System.Windows.Forms.TextBox();
+            this.txtOriOutPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSVN = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtServerPath = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Font = new System.Drawing.Font("宋体", 12F);
+            this.btnReplace.Location = new System.Drawing.Point(564, 50);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(142, 40);
+            this.btnReplace.TabIndex = 0;
+            this.btnReplace.Text = "替换";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // txtProtoPath
+            // 
+            this.txtProtoPath.Location = new System.Drawing.Point(137, 59);
+            this.txtProtoPath.Name = "txtProtoPath";
+            this.txtProtoPath.Size = new System.Drawing.Size(408, 21);
+            this.txtProtoPath.TabIndex = 1;
+            this.txtProtoPath.Text = "D:\\WorkProject\\UnityClient\\Proto\\protofiles\\protofile\\client";
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(10, 64);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(119, 12);
+            this.lbl.TabIndex = 2;
+            this.lbl.Text = "目录下的*.proto文件";
+            // 
+            // btnUnityReplaceFile
+            // 
+            this.btnUnityReplaceFile.Font = new System.Drawing.Font("宋体", 12F);
+            this.btnUnityReplaceFile.Location = new System.Drawing.Point(564, 155);
+            this.btnUnityReplaceFile.Name = "btnUnityReplaceFile";
+            this.btnUnityReplaceFile.Size = new System.Drawing.Size(142, 40);
+            this.btnUnityReplaceFile.TabIndex = 5;
+            this.btnUnityReplaceFile.Text = "Unity替换文件";
+            this.btnUnityReplaceFile.UseVisualStyleBackColor = true;
+            this.btnUnityReplaceFile.Click += new System.EventHandler(this.btnUnityReplaceFile_Click);
+            // 
+            // btnReplaceTxt
+            // 
+            this.btnReplaceTxt.Font = new System.Drawing.Font("宋体", 12F);
+            this.btnReplaceTxt.Location = new System.Drawing.Point(564, 215);
+            this.btnReplaceTxt.Name = "btnReplaceTxt";
+            this.btnReplaceTxt.Size = new System.Drawing.Size(142, 40);
+            this.btnReplaceTxt.TabIndex = 6;
+            this.btnReplaceTxt.Text = "文字替换";
+            this.btnReplaceTxt.UseVisualStyleBackColor = true;
+            this.btnReplaceTxt.Click += new System.EventHandler(this.btnReplaceTxt_Click);
+            // 
+            // lblExcInfo
+            // 
+            this.lblExcInfo.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblExcInfo.AutoSize = true;
+            this.lblExcInfo.Font = new System.Drawing.Font("宋体", 15F);
+            this.lblExcInfo.ForeColor = System.Drawing.Color.Red;
+            this.lblExcInfo.Location = new System.Drawing.Point(288, 263);
+            this.lblExcInfo.Name = "lblExcInfo";
+            this.lblExcInfo.Size = new System.Drawing.Size(49, 20);
+            this.lblExcInfo.TabIndex = 7;
+            this.lblExcInfo.Text = "信息";
+            this.lblExcInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtUnityProtoPath
+            // 
+            this.txtUnityProtoPath.Location = new System.Drawing.Point(137, 168);
+            this.txtUnityProtoPath.Name = "txtUnityProtoPath";
+            this.txtUnityProtoPath.Size = new System.Drawing.Size(408, 21);
+            this.txtUnityProtoPath.TabIndex = 8;
+            this.txtUnityProtoPath.Text = "D:\\WorkProject\\UnityClient\\Unity\\Assets\\Hotfix\\CodeGen\\Proto";
+            // 
+            // txtOriOutPath
+            // 
+            this.txtOriOutPath.Location = new System.Drawing.Point(137, 215);
+            this.txtOriOutPath.Name = "txtOriOutPath";
+            this.txtOriOutPath.Size = new System.Drawing.Size(408, 21);
+            this.txtOriOutPath.TabIndex = 9;
+            this.txtOriOutPath.Text = "D:\\WorkProject\\UnityClient\\Proto\\out\\cs";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 12);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "bat生成的输出目录";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Unity的Proto生成目录";
+            // 
+            // btnSVN
+            // 
+            this.btnSVN.Font = new System.Drawing.Font("宋体", 12F);
+            this.btnSVN.Location = new System.Drawing.Point(564, 4);
+            this.btnSVN.Name = "btnSVN";
+            this.btnSVN.Size = new System.Drawing.Size(142, 40);
+            this.btnSVN.TabIndex = 13;
+            this.btnSVN.Text = "SVN更新替换";
+            this.btnSVN.UseVisualStyleBackColor = true;
+            this.btnSVN.Click += new System.EventHandler(this.btnSVN_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 12F);
+            this.button1.Location = new System.Drawing.Point(564, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 40);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Protoc工具";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtServerPath
+            // 
+            this.txtServerPath.Location = new System.Drawing.Point(137, 17);
+            this.txtServerPath.Name = "txtServerPath";
+            this.txtServerPath.Size = new System.Drawing.Size(408, 21);
+            this.txtServerPath.TabIndex = 15;
+            this.txtServerPath.Text = "D:\\WorkProject\\Common\\Common_Simulation\\Common\\NetProto\\Source";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(76, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "后端目录";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 292);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtServerPath);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSVN);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOriOutPath);
+            this.Controls.Add(this.txtUnityProtoPath);
+            this.Controls.Add(this.lblExcInfo);
+            this.Controls.Add(this.btnReplaceTxt);
+            this.Controls.Add(this.btnUnityReplaceFile);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.txtProtoPath);
+            this.Controls.Add(this.btnReplace);
+            this.Name = "Form1";
+            this.Text = "手搓的~上不了台面的~关于Proto的";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.TextBox txtProtoPath;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Button btnUnityReplaceFile;
+        private System.Windows.Forms.Button btnReplaceTxt;
+        private System.Windows.Forms.Label lblExcInfo;
+        private System.Windows.Forms.TextBox txtUnityProtoPath;
+        private System.Windows.Forms.TextBox txtOriOutPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private Button btnSVN;
+        private Button button1;
+        private TextBox txtServerPath;
+        private Label label4;
+    }
+}
---
+++ ReplaceProtoFile/Form1.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ReplaceProtoFile
+{
+    public partial class Form1 : Form
+    {
+        string mSaveTxtPath;
+        public Form1()
+        {
+            InitializeComponent();
+            mSaveTxtPath = Path.Combine(Application.StartupPath, "zProtoReplace.txt");
+        }
+        void SaveTxtPath()
+        {
+            string contentTxt = this.txtServerPath.Text + "*" + this.txtProtoPath.Text + "*" + this.txtUnityProtoPath.Text + "*" + this.txtOriOutPath.Text;
+            File.WriteAllText(mSaveTxtPath, contentTxt);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(mSaveTxtPath))
+            {
+                string txtContent = File.ReadAllText(mSaveTxtPath);
+
+                var strs = txtContent.Split('*');
+                this.txtServerPath.Text = strs[0];
+                this.txtProtoPath.Text = strs[1];
+                this.txtUnityProtoPath.Text = strs[2];
+                this.txtOriOutPath.Text = strs[3];
+            }
+        }
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            SaveTxtPath();
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            lblExcInfo.Text = "等待 2";
+            var path = txtProtoPath.Text;
+            string[] directories = Directory.GetDirectories(path);
+
+            foreach (string dir in directories)
+            {
+                string[] protoFiles = Directory.GetFiles(dir, "*.proto");           // 搜索每个子文件夹下的所有*.proto文件
+                foreach (string file in protoFiles)
+                {
+                    //等待100毫秒，防止文件操作过快
+                    Task.Delay(100).Wait();
+                    //得到 文件名
+                    string fileName = Path.GetFileName(file);
+
+                    if (fileName.Contains("GamePacketIDs") || fileName.Contains("HallPacketIDs"))
+                    {
+                        string[] lines = File.ReadAllLines(file);
+                        // 替换
+                        lines[0] = @"syntax = ""proto2"";";
+                        lines[1] = @"package Pb;";
+                        lines[2] = @"option csharp_namespace = ""Pb"";";
+
+                        File.WriteAllLines(file, lines); // 将修改后的内容写回文件
+                    }
+                    else if (fileName.Contains("GameSystem"))
+                    {
+                        string[] lines = File.ReadAllLines(file);
+                        // 替换
+                        lines[0] = @"syntax = ""proto2"";";
+                        lines[1] = @"package Pb;";
+                        lines[2] = @"option csharp_namespace = ""Pb"";";
+                        lines[3] = @"import ""common/GamePacketIDs.proto"";";
+                        File.WriteAllLines(file, lines); // 将修改后的内容写回文件
+                    }
+                    else if (fileName.Contains("common") || fileName.Contains("Common"))
+                    {
+                        string[] lines = File.ReadAllLines(file);
+                        // 替换
+                        lines[0] = @"syntax = ""proto2"";";
+                        lines[1] = @"package pb;";
+                        File.WriteAllLines(file, lines); // 将修改后的内容写回文件
+                    }
+                    else if (fileName.Contains("GameBattle.proto"))
+                    {
+                        string[] lines = File.ReadAllLines(file);
+                        // 替换
+                        lines[0] = @"syntax = ""proto2"";";
+                        lines[1] = @"package Pb;";
+                        lines[2] = @"import ""common/Common.proto"";";
+                        lines[3] = @"import ""common/GamePacketIDs.proto"";";
+                        File.WriteAllLines(file, lines); // 将修改后的内容写回文件
+                    }
+                    else if (fileName.Contains("HallAccount.proto"))
+                    {
+                        string[] lines = File.ReadAllLines(file);
+                        // 替换
+                        lines[0] = @"syntax = ""proto2"";";
+                        lines[1] = @"package Pb;";
+                        lines[2] = @"import ""hall/HallPacketIDs.proto"";";
+                        File.WriteAllLines(file, lines); // 将修改后的内容写回文件
+                    }
+                    else if (fileName.Contains("GameLogin.proto"))
+                    {
+                        string[] lines = File.ReadAllLines(file);
+                        // 替换
+                        lines[0] = @"syntax = ""proto2"";";
+                        lines[1] = @"package pb;";
+                        lines[2] = @"import ""common/GamePacketIDs.proto"";";
+                        File.WriteAllLines(file, lines); // 将修改后的内容写回文件
+                    }
+                    else if (fileName.Contains("GamePlayerInfo.proto"))
+                    {
+                        string[] lines = File.ReadAllLines(file);
+                        // 替换
+                        lines[0] = @"syntax = ""proto2"";";
+                        lines[1] = @"package pb;";
+                        lines[2] = @"import ""common/Common.proto"";";
+                        lines[3] = @"import ""common/GamePacketIDs.proto"";";
+                        File.WriteAllLines(file, lines); // 将修改后的内容写回文件
+                    }
+                }
+            }
+
+            lblExcInfo.Text = "完成 2";
+            SaveTxtPath();
+        }
+
+        private void btnUnityReplaceFile_Click(object sender, EventArgs e)
+        {
+            var pathOld = txtUnityProtoPath.Text; //@"D:\WorkProject\UnityClient\Unity\Assets\Hotfix\CodeGen\Proto";
+            var pathNew = txtOriOutPath.Text; //@"D:\WorkProject\UnityClient\Proto\out\cs";
+
+            var fileNames = new List<string>() { "GamePlayerInfo.cs", "GameBattle.cs", "common.cs", "GamePacketIDs.cs", "GameSystem.cs", "HallPacketIDs.cs", "HallAccount.cs", "GameLogin.cs" };
+
+            foreach (var fileName in fileNames)
+            {
+                ReplaceFiles(pathNew, pathOld, fileName);
+            }
+
+            lblExcInfo.Text = "完成 4";
+        }
+
+        public static void ReplaceFiles(string pathNew, string pathOld, string fileName)
+        {
+            // 递归查找并替换文件
+            ReplaceFilesRecursive(pathNew, pathOld, fileName);
+        }
+
+        private static void ReplaceFilesRecursive(string pathNew, string pathOld, string fileName)
+        {
+            // 查找pathNew目录及其子目录下的fileName文件
+            var sourceFiles = Directory.GetFiles(pathNew, fileName, SearchOption.AllDirectories);
+            foreach (var sourceFile in sourceFiles)
+            {
+                // 获取相对路径
+                var relativePath = sourceFile.Substring(pathNew.Length).TrimStart(Path.DirectorySeparatorChar);
+
+                // 构建pathOld目录下的目标文件路径
+                var destinationFile = Path.Combine(pathOld, relativePath);
+
+                // 确保目标文件所在的目录存在
+                var destinationDirectory = Path.GetDirectoryName(destinationFile);
+                if (!Directory.Exists(destinationDirectory))
+                {
+                    Directory.CreateDirectory(destinationDirectory);
+                }
+
+                // 替换文件
+                File.Copy(sourceFile, destinationFile, true);
+            }
+        }
+
+        private async void btnReplaceTxt_Click(object sender, EventArgs e)
+        {
+            lblExcInfo.Text = "等待 5";
+            var pathUnity = txtUnityProtoPath.Text; //@"D:\WorkProject\UnityClient\Unity\Assets\Hotfix\CodeGen\Proto";
+            ModifyMemberAuth(pathUnity);
+
+            await Task.Delay(1000);
+            //1秒后 再执行  别休眠
+            ModifyOther(pathUnity);
+
+            lblExcInfo.Text = "完成 5  去Unity搞事情吧~";
+        }
+        static void ModifyOther(string path)
+        {
+            string[] files = Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories);
+            var strOld = @"[global::ProtoBuf.ProtoMember(2, Name = @""points"")]
+        public global::System.Collections.Generic.List<PointInfo> Points { get; } = new global::System.Collections.Generic.List<PointInfo>();
+
+        [global::ProtoBuf.ProtoMember(3, Name = @""bmove"")]
+        public global::System.Collections.Generic.List<BuildMove> Bmoves { get; } = new global::System.Collections.Generic.List<BuildMove>();";
+
+            var strNew = @"[global::ProtoBuf.ProtoMember(2, Name = @""points"")]
+        public global::System.Collections.Generic.List<PointInfo> Points { get; set;} = new global::System.Collections.Generic.List<PointInfo>();
+
+        [global::ProtoBuf.ProtoMember(3, Name = @""bmove"")]
+        public global::System.Collections.Generic.List<BuildMove> Bmoves { get; set;} = new global::System.Collections.Generic.List<BuildMove>();";
+
+            //替换下 字符串
+            foreach (string file in files)
+            {
+                string content = File.ReadAllText(file);
+                if (content.Contains(strOld))
+                {
+                    content = content.Replace(strOld, strNew);
+                    File.WriteAllText(file, content);
+                }
+            }
+        }
+
+        static void ModifyMemberAuth(string path)
+        {
+            string[] files = Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories);
+            foreach (string filePath in files)
+            {
+                try
+                {
+                    // 读取原始文件内容
+                    string fileContent = File.ReadAllText(filePath);
+                    string searchString = "public global::System.Collections.Generic.List";
+                    string classSearchStr = "public partial class";
+                    string containStr = "Opcodes.CgMsg";
+                    fileContent = UpdateMemberAuth(fileContent, searchString, classSearchStr, containStr, out bool result);
+                    if (result)
+                    {
+                        File.WriteAllText(filePath, fileContent);
+                    }
+                }
+                catch (FileNotFoundException)
+                {
+                    MessageBox.Show("文件未找到。");
+                }
+                catch (IOException e)
+                {
+                    MessageBox.Show($"文件操作时发生错误: {e.Message}");
+                }
+            }
+        }
+
+        static string UpdateMemberAuth(string str, string searchString, string startString, string containStr, out bool result)
+        {
+            result = false;
+            int index = 0;
+            while ((index = str.IndexOf(searchString, index, StringComparison.Ordinal)) != -1)
+            {
+                int startIndex = str.LastIndexOf(startString, index, StringComparison.Ordinal);
+                if (startIndex < 0)
+                {
+                    startIndex = 0;
+                }
+
+                string classStr = str.Substring(startIndex, index - startIndex);
+                if (classStr.Contains(containStr))
+                {
+                    //需要操作
+                    int endIndex = str.IndexOf(");", index, StringComparison.Ordinal);
+                    string leftStr = str.Substring(0, index);
+                    string middleStr = str.Substring(index, endIndex - index).Replace(" { get; }", "");
+                    string rightStr = str.Substring(endIndex, str.Length - endIndex);
+
+                    str = leftStr + middleStr + rightStr;
+                    result = true;
+                }
+                index += searchString.Length;
+            }
+
+            return str;
+        }
+
+        private void btnSVN_Click(object sender, EventArgs e)
+        {
+            // 设置源文件夹和目标文件夹
+            string sourceDir = txtServerPath.Text;//@"D:\WorkProject\Common\Common_Simulation\Common\NetProto\Source";
+            string destDir = txtProtoPath.Text; //@"D:\WorkProject\UnityClient\Proto\protofiles\protofile\client";
+
+            // 更新SVN
+            UpdateSvnDirectory(sourceDir);
+            UpdateSvnDirectory(destDir);
+
+            // 遍历源文件夹内的文件
+            foreach (var file in Directory.GetFiles(sourceDir))
+            {
+                var fileName = Path.GetFileName(file);
+
+                // 遍历目标文件夹内的所有子文件夹
+                foreach (var dir in Directory.GetDirectories(destDir, "*", SearchOption.AllDirectories))
+                {
+                    var targetFile = Path.Combine(dir, fileName);
+
+                    if (File.Exists(targetFile))
+                    {
+                        // 存在同名文件，进行复制并重命名为原名_New
+                        var newFileName = Path.GetFileNameWithoutExtension(fileName) + "_New" + Path.GetExtension(fileName);
+                        var newFilePath = Path.Combine(dir, newFileName);
+                        File.Copy(file, newFilePath, true);
+
+                        // 删除旧文件
+                        File.Delete(targetFile);
+
+                        // 将新文件重命名为原名
+                        File.Move(newFilePath, targetFile);
+                    }
+                }
+            }
+
+            lblExcInfo.Text = "完成 1";
+        }
+
+        private void UpdateSvnDirectory(string dir)
+        {
+            try
+            {
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = "svn.exe",
+                    Arguments = $"update \"{dir}\"",
+                    WindowStyle = ProcessWindowStyle.Hidden,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+                process.StartInfo = startInfo;
+                process.Start();
+                process.WaitForExit();
+                string output = process.StandardOutput.ReadToEnd();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"SVN更新错误: {ex.Message}");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblExcInfo.Text = "等待 3";
+
+            // 设置源目录
+            string directory = txtOriOutPath.Text.Replace(@"\out\cs",""); // @"D:\WorkProject\UnityClient\Proto";
+            string pbToolPath = System.IO.Path.Combine(directory, "PbTool", "pbtool.exe");
+
+            // 创建一个新的进程启动信息
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = pbToolPath,
+                Arguments = "-type=csharp_pbnet",
+                WorkingDirectory = directory, // 设置工作目录
+                UseShellExecute = false, // 如果需要获取输出内容需要设置为false
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            try
+            {
+                // 启动进程
+                using (Process process = Process.Start(startInfo))
+                {
+                    // 等待进程结束
+                    process.WaitForExit();
+
+                    // 获取输出信息
+                    string output = process.StandardOutput.ReadToEnd();
+                    string error = process.StandardError.ReadToEnd();
+
+                    // 显示结果
+                    lblExcInfo.Text = "完成 3";
+                    if (!string.IsNullOrEmpty(error))
+                    {
+                        MessageBox.Show(error, "错误信息");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "错误");
+            }
+        }
+       
+      
+    }
+}
